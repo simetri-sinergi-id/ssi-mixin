0b73a62e23613352baf40c105e96217a7761ca70
mikevhe18 <viriyananda.michael@gmail.com>
2020-12-08 17:55:45 +0700

    [14.0.1.0.0][ssi_multiple_approval]

--- ssi_multiple_approval/models/tier_review.py
+++ ssi_multiple_approval/models/tier_review.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # Copyright 2020 OpenSynergy Indonesia
 # Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
-from openerp.tools.safe_eval import safe_eval as eval
-from openerp.exceptions import Warning as UserError
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
 
 
 class TierReview(models.Model):
     _name = "tier.review"
+    _description = "Tier Review"
 
     status = fields.Selection(
         string="Status",
@@ -18,7 +18,7 @@ class TierReview(models.Model):
             ("draft", "Draft"),
             ("pending", "Pending"),
             ("rejected", "Rejected"),
-            ("approved", "Approved")
+            ("approved", "Approved"),
         ],
         default="draft",
     )
@@ -51,9 +51,7 @@ class TierReview(models.Model):
         comodel_name="res.partner",
         compute="_compute_reviewer_partner_ids",
     )
-    sequence = fields.Integer(
-        string="Tier"
-    )
+    sequence = fields.Integer(string="Tier")
     date = fields.Datetime(
         string="Date",
         readonly=True,
@@ -64,54 +62,40 @@ class TierReview(models.Model):
         readonly=True,
     )
 
-    @api.multi
-    def write(self, vals):
-        for rec in self:
-            if vals.get("status") == "pending":
-                rec._send_email_notification()
-        return super(TierReview, self).write(vals)
-
-    @api.multi
     @api.depends(
         "reviewer_ids",
     )
     def _compute_reviewer_partner_ids(self):
         for rec in self:
-            rec.reviewer_partner_ids =\
-                rec._get_reviewer_partner_ids()
+            rec.reviewer_partner_ids = rec._get_reviewer_partner_ids()
 
-    @api.multi
     def _get_reviewer_partner_ids(self):
         self.ensure_one()
         partner_ids = False
         if self.reviewer_ids:
-            partner_ids = (self.reviewer_ids.mapped("partner_id"))
+            partner_ids = self.reviewer_ids.mapped("partner_id")
         return partner_ids
 
-    @api.multi
     def _get_object(self):
         document_id = self.res_id
         document_model = self.model
 
-        object = self.env[document_model].browse(
-            [document_id]
-        )[0]
+        object = self.env[document_model].browse([document_id])[0]
         return object
 
-    @api.multi
     def _get_localdict(self):
         return {
             "rec": self._get_object(),
             "env": self.env,
         }
 
-    @api.multi
     def _evaluate_python_code(self, python_condition):
         localdict = self._get_localdict()
         result = False
         try:
-            eval(python_condition,
-                 globals_dict=localdict, mode="exec", nocopy=True)
+            safe_eval(
+                python_condition, globals_dict=localdict, mode="exec", nocopy=True
+            )
             result = localdict
         except:
             msg_err = "Error when execute python code"
@@ -119,7 +103,6 @@ class TierReview(models.Model):
 
         return result
 
-    @api.multi
     @api.depends(
         "definition_review_id",
     )
@@ -128,13 +111,11 @@ class TierReview(models.Model):
             list_user = []
             if rec.definition_review_id:
                 review_type = rec.review_type
-                user_ids =\
-                    rec.definition_review_id.reviewer_ids
+                user_ids = rec.definition_review_id.reviewer_ids
                 if user_ids:
                     list_user += user_ids.ids
 
-                group_ids =\
-                    rec.definition_review_id.reviewer_group_ids
+                group_ids = rec.definition_review_id.reviewer_group_ids
                 if group_ids:
                     for group in group_ids:
                         list_user += group.users.ids
@@ -149,37 +130,3 @@ class TierReview(models.Model):
                             msg_err = "No User defines on python code"
                             raise UserError(_(msg_err))
                 rec.reviewer_ids = list(set(list_user))
-
-    @api.multi
-    def get_context(self):
-        self.ensure_one()
-        result = {}
-        return result
-
-    @api.multi
-    def create_mail(self):
-        self.ensure_one()
-        ctx = self.get_context()
-        mail = False
-        obj_template = self.env["email.template"]
-        obj_mail = self.env["mail.mail"]
-
-        template = self.definition_id.email_template_id
-        if template:
-            email_dict = obj_template.with_context(ctx).generate_email(
-                template_id=template.id, res_id=self.res_id)
-            mail = obj_mail.create(email_dict)
-        return mail
-
-    @api.multi
-    def _send_email_notification(self):
-        self.ensure_one()
-        mail = self.create_mail()
-        if mail:
-            mail.write(
-                {"recipient_ids": [(6, 0, self.reviewer_partner_ids.ids)]})
-            try:
-                mail.send()
-            except Exception as error:
-                raise UserError(_(
-                    "Error when sending notification.\n %s") % error)
