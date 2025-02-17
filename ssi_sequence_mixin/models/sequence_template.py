bcdf8f14228adadb2e9ad04e2f8db04a74378d44
Andhitia Rama <andhitia.r@gmail.com>
2025-02-17 16:48:14 +0700

    [UPD] ssi_sequence_mixin
    * Memperbaiki nilai default untuk setiap python code pada sequence template

--- ssi_sequence_mixin/models/sequence_template.py
+++ ssi_sequence_mixin/models/sequence_template.py
@@ -97,7 +97,7 @@ class SequenceTemplate(models.Model):
         string="Python Code",
         default=DEFAULT_PYTHON_CODE
         + "\n#  - result: Return result, the value is boolean."
-        + "\nreturn = True",
+        + "\nresult = True",
         copy=True,
     )
     sequence_selection_method = fields.Selection(
@@ -117,7 +117,7 @@ class SequenceTemplate(models.Model):
     sequence_python_code = fields.Text(
         string="Python Code",
         default=DEFAULT_PYTHON_CODE
-        + "\n#  - result: Return sequence, the value is recordset of sequence.",
+        + "\n#  - sequence: Return sequence, the value is recordset of sequence.",
         copy=True,
     )
     add_custom_prefix = fields.Boolean(
