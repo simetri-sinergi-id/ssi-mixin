a485946eb883b917b190479e8b9fe0b8bd3ffb3a
Andhitia Rama <andhitia.r@gmail.com>
2025-06-12 12:22:50 +0700

    [UPD] ssi_related_attachment_mixin
    * Urutan attachment pd mixin.related_attachment
    * Memperbaiki redefined-buildin

--- ssi_related_attachment_mixin/models/attachment_related_attachment.py
+++ ssi_related_attachment_mixin/models/attachment_related_attachment.py
@@ -10,6 +10,7 @@ from odoo.tools.safe_eval import safe_eval
 class AttachmentRelatedAttachment(models.Model):
     _name = "attachment.related_attachment"
     _description = "Related Attachment"
+    _order = "template_id, template_detail_id"
 
     model = fields.Char(
         string="Related Document Model",
@@ -133,8 +134,8 @@ class AttachmentRelatedAttachment(models.Model):
         document_id = self.res_id
         document_model = self.model
 
-        object = self.env[document_model].browse([document_id])[0]
-        return object
+        mixin_record = self.env[document_model].browse([document_id])[0]
+        return mixin_record
 
     def _get_localdict(self):
         return {
