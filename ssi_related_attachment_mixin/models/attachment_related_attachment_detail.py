2ea7c7c24d2c91b47c06fe09e6db8d958a70ed29
mikevhe18 <viriyananda.michael@gmail.com>
2025-07-15 12:01:10 +0700

    [ssi_related_attachment_mixin] 14.0.2.10.1

--- ssi_related_attachment_mixin/models/attachment_related_attachment_detail.py
+++ ssi_related_attachment_mixin/models/attachment_related_attachment_detail.py
@@ -2,7 +2,9 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools.safe_eval import test_python_expr
 
 
 class AttachmentRelatedAttachmentTemplateDetail(models.Model):
@@ -59,3 +61,13 @@ class AttachmentRelatedAttachmentTemplateDetail(models.Model):
         string="Python Code",
         default="""# Available locals:\n#  - rec: current record""",
     )
+
+    @api.constrains(
+        "python_code",
+    )
+    def _check_python_code(self):
+        for action in self.sudo().filtered("python_code"):
+            msg = test_python_expr(expr=action.python_code.strip(), mode="exec")
+            if msg:
+                msg1 = "Template Detail:\n"
+                raise ValidationError(msg1 + msg)
