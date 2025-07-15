6f94fe2485c0e4ad0ed076d2fa272aa2035d1c2b
mikevhe18 <viriyananda.michael@gmail.com>
2025-07-15 10:51:15 +0700

    [ssi_state_change_constrain_mixin] 14.0.1.7.2

--- ssi_state_change_constrain_mixin/__manifest__.py
+++ ssi_state_change_constrain_mixin/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "State Change Constrain Mixin",
-    "version": "14.0.1.7.1",
+    "version": "14.0.1.7.2",
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
