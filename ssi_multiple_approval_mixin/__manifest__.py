ec638a4adb37bde813d6c2ede74ab6d877ff0580
mikevhe18 <viriyananda.michael@gmail.com>
2025-07-15 11:11:32 +0700

    [ssi_multiple_approval_mixn] 14.0.1.10.2

--- ssi_multiple_approval_mixin/__manifest__.py
+++ ssi_multiple_approval_mixin/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Multiple Approval",
-    "version": "14.0.1.10.1",
+    "version": "14.0.1.10.2",
     "category": "Administration",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
--- ssi_multiple_approval_mixin/models/approval_template_detail.py
+++ ssi_multiple_approval_mixin/models/approval_template_detail.py
@@ -2,7 +2,9 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools.safe_eval import test_python_expr
 
 
 class ApprovalTemplateDetail(models.Model):
@@ -54,3 +56,13 @@ class ApprovalTemplateDetail(models.Model):
         string="Python Code",
         default="""# Available locals:\n#  - rec: current record""",
     )
+
+    @api.constrains(
+        "python_code",
+    )
+    def _check_python_code(self):
+        for action in self.sudo().filtered("python_code"):
+            msg = test_python_expr(expr=action.python_code.strip(), mode="exec")
+            if msg:
+                msg1 = "Template Detail:\n"
+                raise ValidationError(msg1 + msg)
