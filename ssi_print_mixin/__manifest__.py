aa120fac353ff5c25c47f8fbf0627541ef997667
mikevhe18 <viriyananda.michael@gmail.com>
2025-07-17 19:00:59 +0700

    [ssi_print_mixin] 18.0.1.0.2

--- ssi_print_mixin/__manifest__.py
+++ ssi_print_mixin/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Print Policy Mixin",
     "author": "PT. Simetri Sinergi Indonesia",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "website": "https://github.com/open-synergy/ssi-mixin",
     "license": "AGPL-3",
     "installable": True,
