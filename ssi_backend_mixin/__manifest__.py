83a116aa789fc6685a86312befe15dc6594be794
mikevhe18 <viriyananda.michael@gmail.com>
2025-08-26 13:31:00 +0700

    [ssi_backend_mixin] 14.0.1.0.0

---
+++ ssi_backend_mixin/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Backend Mixin",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_master_data_mixin",
+    ],
+    "data": [
+        "views/mixin_backend_views.xml",
+        "views/res_company_views.xml",
+    ],
+}
