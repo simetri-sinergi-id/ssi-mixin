928bb059b28263c2d8bdcbdc117c80b2d1503ad6
Andhitia Rama <andhitia.r@gmail.com>
2026-04-11 17:39:09 +0700

    [UPD] ssi_schema_parser_mixin
    * Update docstring

--- ssi_schema_parser_mixin/models/schema_parser_category.py
+++ ssi_schema_parser_mixin/models/schema_parser_category.py
@@ -2,6 +2,11 @@ from odoo import models
 
 
 class SchemaParserCategory(models.Model):
+    """
+    Master-data model (inheriting ``mixin.master_data``) that categorises
+    ``schema_parser`` records into logical groups.
+    """
+
     _name = "schema_parser_category"
     _description = "Schema Parser Category"
     _inherit = "mixin.master_data"
