5071f02bfcb1344769da25c5fe594ba2d4b68baa
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-18 23:23:34 +0700

    [MIG] ssi_product_line_mixin: Migration to 15.0

--- ssi_product_line_mixin/__manifest__.py
+++ ssi_product_line_mixin/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Line Mixin",
-    "version": "14.0.1.3.0",
+    "version": "15.0.1.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
