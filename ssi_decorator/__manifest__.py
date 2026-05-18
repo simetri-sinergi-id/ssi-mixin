02734fc40738c25ac9fff271a3df1a28b1cb6b2a
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-18 20:42:15 +0700

    [MIG] ssi_decorator: Migration to 15.0

--- ssi_decorator/__manifest__.py
+++ ssi_decorator/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "SSI - Decorator",
-    "version": "14.0.1.5.1",
+    "version": "15.0.1.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
