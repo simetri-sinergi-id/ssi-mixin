a60b436f982fd8b83bcaad04c6d250b665cd0c21
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-19 19:28:55 +0700

    [ssi_transaction_ready_mixin] 15.0.1.0.0

--- ssi_transaction_ready_mixin/__manifest__.py
+++ ssi_transaction_ready_mixin/__manifest__.py
@@ -1,11 +1,10 @@
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Transaction Mixin - Ready to Process State",
     "version": "15.0.1.0.0",
     "website": "https://github.com/simetri-sinergi-id/ssi-mixin",
-    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "author": "PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
