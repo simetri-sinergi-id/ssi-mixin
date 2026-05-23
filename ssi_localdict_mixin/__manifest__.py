bad054180e9da9a8374eb67064d802c1c0852180
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-23 14:59:18 +0700

    [IMP] ssi_m2o_configurator_mixin: pre-commit execution

--- ssi_localdict_mixin/__manifest__.py
+++ ssi_localdict_mixin/__manifest__.py
@@ -1,11 +1,10 @@
-# Copyright 2023 OpenSynergy Indonesia
-# Copyright 2023 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Localdict Mixin",
     "version": "15.0.1.0.0",
     "website": "https://github.com/simetri-sinergi-id/ssi-mixin",
-    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "author": "PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
