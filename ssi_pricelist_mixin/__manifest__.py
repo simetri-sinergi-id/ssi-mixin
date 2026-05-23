31a267d381afcec546d0a2eb2383556e1ee8fdac
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-23 15:43:35 +0700

    [MIG] ssi_pricelist_mixin: Migration to 15.0

--- ssi_pricelist_mixin/__manifest__.py
+++ ssi_pricelist_mixin/__manifest__.py
@@ -4,11 +4,11 @@
 
 {
     "name": "Pricelist Mixin",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/simetri-sinergi-id/ssi-mixin",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "depends": [
         "product",
     ],
