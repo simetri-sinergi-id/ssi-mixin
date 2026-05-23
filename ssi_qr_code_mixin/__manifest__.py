d4120c3f0211b020639b26656c9a4f00e0a52262
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-23 16:12:45 +0700

    [MIG] ssi_qr_code_mixin: Migration to 15.0

--- ssi_qr_code_mixin/__manifest__.py
+++ ssi_qr_code_mixin/__manifest__.py
@@ -4,11 +4,11 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "QR Code Mixin",
-    "version": "14.0.2.1.1",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/simetri-sinergi-id/ssi-mixin",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "depends": [
         "ssi_decorator",
     ],
