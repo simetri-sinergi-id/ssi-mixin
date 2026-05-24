a39c27f1c9ba67de0eae722a509339251591dfb5
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-24 11:35:57 +0700

    [UPD] Implementasi ssi_accounting_entry_mixin, ssi_custom_information_mixin

--- test_ssi_transaction_mixin/__manifest__.py
+++ test_ssi_transaction_mixin/__manifest__.py
@@ -17,6 +17,8 @@
         "ssi_transaction_terminate_mixin",
         "ssi_product_line_price_mixin",
         "ssi_company_currency_mixin",
+        "ssi_accounting_entry_mixin",
+        "ssi_custom_information_mixin",
     ],
     "data": [
         "security/ir.model.access.csv",
