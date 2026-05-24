0dd334862ec721b62798dd2b95c17bf0adcd6f0d
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-24 11:49:14 +0700

    [ssi_localdict_mixin] 15.0.1.0.0

--- ssi_localdict_mixin/models/__init__.py
+++ ssi_localdict_mixin/models/__init__.py
@@ -1,6 +1,5 @@
-# Copyright 2023 OpenSynergy Indonesia
-# Copyright 2023 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import (
     mixin_localdict,
