6e05169ae60751ef53d5a71e45f6872be12163b6
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-24 12:05:23 +0700

    [ssi_qr_code_mixin] 15.0.1.0.0

--- ssi_qr_code_mixin/__manifest__.py
+++ ssi_qr_code_mixin/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "QR Code Mixin",
-    "version": "14.0.2.1.1",
+    "version": "15.0.1.0.0",
     "website": "https://github.com/simetri-sinergi-id/ssi-mixin",
-    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "author": "PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "depends": [
         "ssi_decorator",
     ],
