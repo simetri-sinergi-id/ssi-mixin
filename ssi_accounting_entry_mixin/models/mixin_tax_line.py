23d3862ae22cb965bcb16390b219960b7ac5ee07
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-24 11:28:19 +0700

    [ssi_accounting_entry_mixin] 15.0.1.0.0

--- ssi_accounting_entry_mixin/models/mixin_tax_line.py
+++ ssi_accounting_entry_mixin/models/mixin_tax_line.py
@@ -1,6 +1,5 @@
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
@@ -29,42 +28,52 @@ class MixinTaxLine(models.AbstractModel):
     name = fields.Char(
         string="Description",
         required=True,
+        help="Short description of this tax line.",
     )
     tax_id = fields.Many2one(
         string="Tax",
         comodel_name="account.tax",
         required=True,
+        help="Tax applied on this line.",
     )
     account_id = fields.Many2one(
         string="Account",
         comodel_name="account.account",
         required=True,
+        help="General ledger account used to post the tax amount.",
     )
     analytic_account_id = fields.Many2one(
         string="Analytic Account",
         comodel_name="account.analytic.account",
+        help="Analytic account for cost/revenue allocation of this tax line.",
     )
     currency_id = fields.Many2one(
         string="Currency",
         comodel_name="res.currency",
+        help="Currency used for this tax line.",
     )
     base_amount = fields.Monetary(
         string="Base Amount",
         currency_field="currency_id",
         required=True,
+        help="Taxable base amount for this tax line.",
     )
     tax_amount = fields.Monetary(
         string="Tax Amount",
         currency_field="currency_id",
         required=True,
+        help="Computed tax amount for this tax line.",
     )
     manual = fields.Boolean(
         string="Manual",
         default=True,
+        help="When checked, this tax line is entered manually "
+        "and not recomputed automatically.",
     )
     move_line_id = fields.Many2one(
         string="Journal Item",
         comodel_name="account.move.line",
         readonly=True,
         copy=False,
+        help="Journal item generated when this tax line is posted.",
     )
