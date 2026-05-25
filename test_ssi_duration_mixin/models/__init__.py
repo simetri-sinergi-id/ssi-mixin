f24dc26e42b985964a9d388a2423a791067c3177
mikevhe18 <viriyananda.michael@gmail.com>
2026-05-25 13:45:37 +0700

    [DEL] test_ssi_duration_mixin

--- test_ssi_duration_mixin/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import (
-    test_master_data_date_duration,
-    test_transaction_date_duration,
-)
